7dd2c626ceeb30c88c50cf5d0a0d8763bcbb5d77
Bogdan Amihaesie <bogami@amazon.com>
2017-05-31 12:51:20 -0700

    Make GCE module use joke engine

--- jokeengine/src/main/java/com/example/JokeProvider.java
+++ jokeengine/src/main/java/com/example/JokeProvider.java
@@ -1,4 +1,8 @@
 package com.example;
 
 public class JokeProvider {
+
+    public String getJoke() {
+        return "Can a kangaroo jump higher than a house? Of course, a house doesn’t jump at all.";
+    }
 }
